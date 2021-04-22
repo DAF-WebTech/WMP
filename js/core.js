16a1d05f7603e24b83fe275ae9ee554f30366c56
David Porter <david.porter@fastmail.com>
2021-04-22 13:27:07 +1000

    add forEach to NodeList

--- js/core.js
+++ js/core.js
@@ -1,5 +1,13 @@
 "use strict";
 
+if (!NodeList.prototype.forEach) {
+  NodeList.prototype.forEach = function(fn, scope) {
+    for(var i = 0, len = this.length; i < len; ++i) {
+      fn.call(scope, this[i], i, this);
+    }
+  }
+}
+
 window.QG = window.QG || {};
 
 /*==-------------------------------------------------------------------====
